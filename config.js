b7b3ade18848121afc12c7942b7ac3c95b15ee81
Tzikas <niko.tzikas@gmail.com>
2018-09-27 17:13:51 -0400

    tessss

---
+++ config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/reactadsfadsf-universal';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
+exports.PORT = process.env.PORT || 8080;
+
+exports.AUTH0_DOMAIN = process.env.AUTH0_DOMAIN || 'nikoniko.auth0.com';
+;
+exports.AUTH0_API_AUDIENCE = process.env.AUTH0_API_AUDIENCE || 'https://www.puppycakes.com';
---
+++ routes/users.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const { User } = require('../models/user');
+const { checkJwt } = require('../middlewares/auth');
+const Subscriber = require('../utils/Subscriber');
+
+const router = express.Router();
+
+router.post('/update', checkJwt, async function(req, res){
+
+    const user = {
+        user_id: req.user.sub,
+        email: req.body.email
+    }
+      
+    try {
+        const user_db = await User.findOneAndUpdate(
+          {user_id: user.user_id },
+          user,
+          {upsert: true},
+          function(err){
+            if (err){
+              console.log(err);
+            }
+          }
+        );
+        res.status(200).json(user_db);
+    }
+    catch(e)
+    {
+      console.error(e);
+      res.status(500).json('Internal server error.');
+    }
+
+})
+
+module.exports = router;
