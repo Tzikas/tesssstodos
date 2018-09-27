2bb4ca49f2532f61eef6fca4529fdfff268484da
Tzikas <niko.tzikas@gmail.com>
2018-09-27 17:22:59 -0400

    eek

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/reactadsfadsf-universal';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://todos:todos8888@ds117623.mlab.com:17623/expotodos' //'mongodb://localhost/sup-universal';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
 exports.PORT = process.env.PORT || 8080;
 
 exports.AUTH0_DOMAIN = process.env.AUTH0_DOMAIN || 'nikoniko.auth0.com';
 ;
 exports.AUTH0_API_AUDIENCE = process.env.AUTH0_API_AUDIENCE || 'https://www.puppycakes.com';
+
